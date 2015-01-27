cdd70ee14cc4b9ace6f2b4ce549e00b37f9b01b2
johnymoo <johnymoo12@gmail.com>
2015-01-27 15:56:54 +0800

    output CO2 in ppm, VOC in ppb

--- VOC/MiCS-VZ-86/MiCS-VZ-86.ino
+++ VOC/MiCS-VZ-86/MiCS-VZ-86.ino
@@ -11,11 +11,14 @@
 #define PWM_PIN 3
 
 volatile unsigned long pwm_value = 0;
-volatile unsigned long pwm_co2 = 0;
-volatile unsigned long pwm_voc = 0;
+volatile unsigned long pwm_value1 = 0;
+volatile unsigned long pwm_value2 = 0;
 volatile unsigned long prev_time = 0;
+int pwm_co2 = 0;
+int pwm_voc = 0;
 
 boolean channel = false;
+unsigned long tmp;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -28,19 +31,32 @@ void setup() {
 void loop() {
   delay(1000);
   
+  // make sure pwm_value 1 > pwm_value2
+  if( pwm_value1 < pwm_value2 )
+  {
+    tmp = pwm_value1;
+    pwm_value1 = pwm_value2;
+    pwm_value2 = tmp;
+  }
+  pwm_co2 = 40*pwm_value1-1800;
+  pwm_voc = 25*pwm_value2-125;
+    
+  //Serial.print(pwm_value1);
+  //Serial.print(" ");
+  //Serial.println(pwm_value2);
   Serial.print("CO2: ");
-  Serial.println((pwm_co2>pwm_voc?pwm_co2:pwm_voc));
+  Serial.println(pwm_co2);
   Serial.print("VOC: ");
-  Serial.println((pwm_voc<pwm_co2?pwm_voc:pwm_co2));
+  Serial.println(pwm_voc);
 }
 
 void falling() {
   pwm_value = (millis() - prev_time)*3;
   if( !channel ){
-    pwm_co2 = pwm_value;
+    pwm_value1 = pwm_value;
   } 
   else {
-    pwm_voc = pwm_value;
+    pwm_value2 = pwm_value;
   }
    channel = !channel; 
   attachInterrupt(1, rising, RISING);
