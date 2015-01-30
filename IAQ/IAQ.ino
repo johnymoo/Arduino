12ce371745ddeaa5bb35e740145f5633a6234903
johnymoo <johnymoo12@gmail.com>
2015-01-30 17:39:26 +0800

    merge all sensors readings in one instance; including required libraries

--- IAQ/IAQ.ino
+++ IAQ/IAQ.ino
@@ -8,26 +8,44 @@
  by Chris Wang
  */
 #include <Wire.h>
+#include <digitalWriteFast.h>
+#include <PinChangeInt.h>
+#include <AAS.h>
 
+char delimiter[]=",";
+
+// for SW-PWM-01A Dust sensor
+#define DUST_PWM_PIN1  10
+#define DUST_PWM_PIN2  11
+unsigned long TimeOfsample = 0;
+DATA DUST_Small_Pin1; 
+DATA DUST_Small_Pin2;
+
+// for Telaire 6713 CO2 sensor
 #define ADDR_6713  0x15 // default I2C slave address
-
 int ppm = 0;
-#define PWM_PIN 3
 
-volatile unsigned long pwm_value = 0;
-volatile unsigned long pwm_value1 = 0;
-volatile unsigned long pwm_value2 = 0;
-volatile unsigned long prev_time = 0;
-int pwm_co2 = 0;
-int pwm_voc = 0;
+// for SGX VOC VZ86 sensors
+// the pin that will read in PWM signal
+#define VOC_IN_PIN0 0
+#define VOC_IN_PIN1 5
+#define VOC_IN_PIN2 2
+#define VOC_IN_PIN3 3
+#define VOC_IN_PIN4 4
 
-boolean channel = false;
-unsigned long tmp;
-unsigned long cnt = 0;
+volatile unsigned long prevTimeShared[5]={0};
+volatile unsigned long pwmValue1Shared[5]={0};
+volatile unsigned long pwmValue2Shared[5]={0};
+volatile boolean pwmChannelShared[5]={false}; 
+int pinNumber[5] = {VOC_IN_PIN0, VOC_IN_PIN1, VOC_IN_PIN2, VOC_IN_PIN3, VOC_IN_PIN4};
+unsigned long time=0;
+
+
+// ------------ Function -------------------//
 
 // send request to read current gas measurement in ppm
 // return status: 0 success
-byte queryPPM()
+byte queryCO2()
 {
   int bytes;
 
@@ -51,7 +69,7 @@ byte queryPPM()
 }
 
 // read report of current gas measurement in ppm
-int readPPM()
+int readCO2PPM()
 {
   byte func_code, byte_count, MSB, LSB;
   func_code = 0;
@@ -73,82 +91,232 @@ int readPPM()
    Serial.println(MSB);
     Serial.print("LSB: ");
    Serial.println(LSB);*/
-   
-   // ppm = MSB*256 + LSB
+
    return ((MSB<<8)|LSB);
+}
+
+void calcPin0PWM()
+{
+  int i = 0;
+  if( digitalReadFast(VOC_IN_PIN0) == HIGH )
+  {
+    prevTimeShared[i] = micros();
+  }
+  else
+  {
+    if( !pwmChannelShared[i] )
+    {
+      pwmValue1Shared[i] = (micros() - prevTimeShared[i]);
+    }
+    else{
+      pwmValue2Shared[i] = (micros() - prevTimeShared[i]);
+    }
+    pwmChannelShared[i] = !pwmChannelShared[i];
+  }
+}
+
+void calcPin1PWM()
+{
+  int i = 1;
+  if( digitalReadFast(VOC_IN_PIN1) == HIGH )
+  {
+    prevTimeShared[i] = micros();
+  }
+  else
+  {
+    if( !pwmChannelShared[i] )
+    {
+      pwmValue1Shared[i] = (micros() - prevTimeShared[i]);
+    }
+    else{
+      pwmValue2Shared[i] = (micros() - prevTimeShared[i]);
+    }
+    pwmChannelShared[i] = !pwmChannelShared[i];
+  }
+}
+
+void calcPin2PWM()
+{
+  int i = 2;
+  if( digitalReadFast(VOC_IN_PIN2) == HIGH )
+  {
+    prevTimeShared[i] = micros();
+  }
+  else
+  {
+    if( !pwmChannelShared[i] )
+    {
+      pwmValue1Shared[i] = (micros() - prevTimeShared[i]);
+    }
+    else{
+      pwmValue2Shared[i] = (micros() - prevTimeShared[i]);
+    }
+    pwmChannelShared[i] = !pwmChannelShared[i];
+  }
+}
+
+void calcPin3PWM()
+{
+  int i = 3;
+  if( digitalReadFast(VOC_IN_PIN3) == HIGH )
+  {
+    prevTimeShared[i] = micros();
+  }
+  else
+  {
+    if( !pwmChannelShared[i] )
+    {
+      pwmValue1Shared[i] = (micros() - prevTimeShared[i]);
+    }
+    else{
+      pwmValue2Shared[i] = (micros() - prevTimeShared[i]);
+    }
+    pwmChannelShared[i] = !pwmChannelShared[i];
+  }
+}
+
+void calcPin4PWM()
+{
+  int i = 4;
+  if( digitalReadFast(VOC_IN_PIN4) == HIGH )
+  {
+    prevTimeShared[i] = micros();
+  }
+  else
+  {
+    if( !pwmChannelShared[i] )
+    {
+      pwmValue1Shared[i] = (micros() - prevTimeShared[i]);
+    }
+    else{
+      pwmValue2Shared[i] = (micros() - prevTimeShared[i]);
+    }
+    pwmChannelShared[i] = !pwmChannelShared[i];
+  }
+}
+
+unsigned int GetDUST_SmallPin1(void)  //??μ?D?·?3?(1-2um)?¨?è
+{  
+  return (unsigned int)(GetDUSTReading(&DUST_Small_Pin1));
+}
+unsigned int GetDUST_SmallPin2(void)  //??μ?D?·?3?(1-2um)?¨?è
+{  
+  return (unsigned int)(GetDUSTReading(&DUST_Small_Pin2));
 }
 
 // the setup function runs once when you press reset or power the board
 void setup() {
+  int i;
+  //initialize
   Serial.begin(115200);
   Wire.begin();
-  delay(5000);
-  Serial.println("Time\tCO2_PWM(%)\tVOC_PWM(%)\tCO2(ppm)\tVOC(ppa)\tAAS_CO2(ppm)");
-  attachInterrupt(1, rising, RISING);
+  DataInit(&DUST_Small_Pin1 ,0); 
+  DataInit(&DUST_Small_Pin2 ,0);
+  
+  pinMode(DUST_PWM_PIN1, INPUT);
+  pinMode(DUST_PWM_PIN2, INPUT);
+  
+  // print header
+  Serial.print("Time");
+  for( i=0; i<5; i++)
+  {
+    pinMode(pinNumber[i],INPUT);
+    Serial.print(delimiter);
+    Serial.print("VOC");
+    Serial.print(i); 
+    Serial.print(delimiter);
+    Serial.print("CO2");
+    Serial.print(i);
+  }
+  Serial.print(delimiter);
+  Serial.print("AAS_CO2");
+  Serial.print(delimiter);
+  Serial.print("Dust1");
+  Serial.print(delimiter);
+  Serial.print("Dust2");
+  
+  //allow sensors to warm up
+  delay(10000);    
+  
+  // attach interrupt for VOC
+  PCintPort::attachInterrupt(VOC_IN_PIN0, calcPin0PWM,CHANGE);
+  PCintPort::attachInterrupt(VOC_IN_PIN1, calcPin1PWM,CHANGE);
+  PCintPort::attachInterrupt(VOC_IN_PIN2, calcPin2PWM,CHANGE);
+  PCintPort::attachInterrupt(VOC_IN_PIN3, calcPin3PWM,CHANGE);
+  PCintPort::attachInterrupt(VOC_IN_PIN4, calcPin4PWM,CHANGE);
 }
 
-void readVOC()
-{
-  static unsigned long co2_value, voc_value;
-  noInterrupts(); // turn interrupts off quickly while we take local copies of the shared variables
-  // make sure pwm_value 1 > pwm_value2
-  co2_value = (pwm_value1 > pwm_value2)?pwm_value1:pwm_value2;
-  voc_value = (pwm_value1 < pwm_value2)?pwm_value1:pwm_value2;
-  interrupts(); 
-  
-  pwm_co2 = 40*co2_value/1000.0-1800;
-  pwm_voc = 25*voc_value/1000.0-125;
-  
-  //Serial.print("SGX in PWM (%): CO2 ");  
-  Serial.print(co2_value/1000.0);
-  //Serial.print(" VOC ");
-  Serial.print("\t");
-  Serial.print(voc_value/1000.0);
-  //Serial.print("CO2: ");
-  Serial.print("\t");
-  Serial.print(pwm_co2);
-  //Serial.print(" ppm ");
-  //Serial.print(" VOC: ");
-  Serial.print("\t");
-  Serial.print(pwm_voc);
-  //Serial.println(" ppa");
-}
 
 // the loop function runs over and over again forever
 void loop() {
-  if( queryPPM() == 0 )
-  {
-    ppm = readPPM();
+  static unsigned long pwmValueCO2[5];
+  static unsigned long pwmValueVOC[5];
+  static int TimesOfInt = 0, P1TimesOfLowSignalPin1 = 0, P1TimesOfLowSignalPin2=0;
+  int i;
   
-    //Serial.print("6713: ");
-    //Serial.print(ppm);
-    //Serial.println(" ppm");
-    Serial.print(cnt);
-    Serial.print("\t");
-     readVOC();
-     Serial.print("\t");
-     Serial.println(ppm);
-	// 6713 sample CO2 every 5 seconds
-    delay(5000);
-    cnt++;
+  //sample PM2.5
+  if( digitalReadFast(DUST_PWM_PIN1) == LOW )
+  {
+    P1TimesOfLowSignalPin1++;
   }
-  interrupts();
+  if( digitalReadFast(DUST_PWM_PIN2) == LOW )
+  {
+    P1TimesOfLowSignalPin2++;
+  }
+  
+  TimesOfInt++; 
+  
+  if(TimesOfInt%1000 == 0 )   // update PM2.5 window every second
+  {     		
+    TimeOfsample++;
+					 
+    UpdateWindowData(&DUST_Small_Pin1 , P1TimesOfLowSignalPin1 ) ;
+    UpdateWindowData(&DUST_Small_Pin2 , P1TimesOfLowSignalPin2 ) ;	
+    
+    P1TimesOfLowSignalPin1 = 0;
+    P1TimesOfLowSignalPin2 = 0;
+  }
+  
+  // output VOC, CO2, PM2.5 reading every 5 seconds
+  if( TimeOfsample == 5 )
+  {
+    TimeOfsample = 0;
+    TimesOfInt = 0;
+    
+    //read VOC
+    noInterrupts();
+    for( i=0; i<5; i++ )
+    {
+      pwmValueCO2[i] = (pwmValue1Shared[i] > pwmValue2Shared[i])?pwmValue1Shared[i]:pwmValue2Shared[i];
+      pwmValueVOC[i] = (pwmValue1Shared[i] < pwmValue2Shared[i])?pwmValue1Shared[i]:pwmValue2Shared[i];
+    }
+    interrupts();
+    
+    //read CO2
+    if( queryCO2() == 0 )
+    {
+      ppm = readCO2PPM();
+    }
+    
+    //output all readings
+    Serial.print(time); // Time
+    //VOC
+    for( i=0; i<5; i++ )
+    {
+      Serial.print(delimiter);
+      Serial.print(pwmValueVOC[i]*3/1000.0);
+      Serial.print(delimiter);
+      Serial.print(pwmValueCO2[i]*3/1000.0);
+    }
+    Serial.print(delimiter);
+    Serial.print(ppm); // CO2
+    Serial.print(delimiter);
+    Serial.print(GetDUST_SmallPin1()); //Dust 1
+    Serial.print(delimiter);
+    Serial.println(GetDUST_SmallPin2());//Dust 2
+  }
+  
+  delay(1);
 }
 
-void falling() {
-  pwm_value = (micros() - prev_time)*3;
-  if( !channel ){
-    pwm_value1 = pwm_value;
-  } 
-  else {
-    pwm_value2 = pwm_value;
-  }
-   channel = !channel; 
-  attachInterrupt(1, rising, RISING);
-}
-
-void rising(){
-  prev_time = micros();
-  attachInterrupt(1, falling, FALLING);
-}
 
--- library/AAS/AAS.h
+++ library/AAS/AAS.h
@@ -13,4 +13,75 @@ typedef struct
 	DataType data[WIDE];	    
 }DATA;
 
+
+void DataInit(DATA * d, DataType InitValue)	//?¨¢11¨¬?¨ºy?Y3?¨º??¡¥,???¨¢11¨¬??¨²¦Ì??a??3?¨º??¡¥?a???¡§?¦Ì¡ê?¨°?¡ã??a0 
+{
+	int i = 0;
+ 	d->position = 0;
+ 	for(i = 0 ; i < WIDE ; i++)
+		d->data[i] = InitValue;
+} 	
+
+void UpdateWindowData(DATA * d, DataType NewData)  //?????¡¥¡ä¡ã?¨²¨¤???¦Ì?¨ºy?Y??DD?¨¹D? 
+{
+	int i = 0, pos = 0;
+	double sum = 0.0 ,residue  = 0.0; 
+	DataType Average = 0; 				//???¨´?¦Ì 
+	
+	NewData = NewData*60;//¡ê?¡À?¨¢?¡Á?¡ä¨®?¦Ì?¨¦?¨¹¨º?1000¡ê?3?¨°?60??o¨®¡ê?¡Á?¡ä¨®?¦Ì60000¡ê??a?¨´¡Á?¡ê??¨¦¨°????¨®???¨¨¡ê???¨¦¨´o¨®?¨²??¨ºy????¡ä¨°¨¤¡ä¦Ì??¨®2?
+		
+	if((NewData<(MAX*60)) && (NewData>(MIN*60)))  	   //¨ºy?Yo?¡¤¡§ 
+	{
+		d->data[d->position] =  NewData ;   
+	}
+	else								         //¨ºy?Y2?o?¡¤¡§¡ê?¨®|???¨´¨¬¡¥¦Ì?Nearby??¨ºy?Y¦Ì¡À?D 
+	{
+		sum = NewData;					   //NewData2?¨®????¨´ 
+		
+		pos = d->position ;			   //??¦Ì¡À?¡ã????¨¨?3?¨¤¡ä 
+		for(i = 0 ; i < NEARBY-1 ; i++)      //?¨®???¨´?¦Ì 
+		{
+			pos	= (pos+1)%WIDE;         //¨¨¡¤¡À¡ê¨ºy¡Á¨¦??¡À¨º2?¨°?3? 
+			
+			sum = sum + d->data[pos];  	
+			
+		}
+		Average = (DataType)(sum/(NEARBY*1.0));  //?¨®???¨´?¦Ì
+	 
+		d->data[d->position] = Average;                //¡Á?D?¨°???¨ºy?Y¨®????¨´?¦Ì¡ä¨²¨¬? 
+		
+		residue  = (NewData*1.0 - Average*1.0 )/(NEARBY-1.0);//¨º¡ê¨®¨¤¦Ì?¨ºy?Y¡ê?D¨¨¨°a??¨¬¡¥¦Ì???o¨®nearby??¨ºy?Y¦Ì¡À?D 
+			
+		pos = d->position ;			       			   //??¦Ì¡À?¡ã????¨¨?3?¨¤¡ä 	
+		for(i = 0 ; i < NEARBY-1 ; i++) 	     //?¨´¨¬¡¥¨ºy?Y 
+		{
+			pos	= (pos + 1)%WIDE;  			         //¨¨¡¤¡À¡ê¨ºy¡Á¨¦??¡À¨º2?¨°?3? 
+			
+			if((d->data[pos]*1.0 + residue ) <= 0.0) //¨¨¡¤¡À¡ê¨ºy?Y2?¨°?3?	
+			{
+				d->data[pos] = 0 ;  	
+			} 
+			else
+			{
+				d->data[pos] = (DataType)(d->data[pos]*1.0 + residue ) ;   //¨º¦Ì??¨ºy?Y?¨´¨¬¡¥ 
+			}
+		}
+	} 
+
+	d->position = (d->position+1)%WIDE;  //????????o¨®¨°? 	
+}
+
+
+double GetDUSTReading(DATA * d)            //??¨¨??¨¢11¨¬??¨²¨ºy?Y¦Ì????¨´?¦Ì  ¦Ì£¤??¡êoug/M3
+{
+	int i = 0;
+	double sum = 0.0;
+	for(i = 0; i < WIDE ; i++)
+	{
+		sum += d->data[i];
+	}
+	
+	return (sum*2.0/(WIDE*60.0));  //¦Ì£¤??¡êoug/m3
+}
+
 #endif
